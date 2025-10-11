694f562f7e76999a435683c5c9f25a546da96ffd
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-11 19:08:57 +0200

    refactor: remove useless import

--- src/pages/translator/translator-page.ts
+++ src/pages/translator/translator-page.ts
@@ -1,12 +1,10 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { translatorService } from './translator.service';
 import '../../components/app-layout';
 import './language-selection/language-selection';
 
 @customElement('translator-page')
 export class TranslatorPage extends LitElement {
-  readonly #service = translatorService;
 
   protected createRenderRoot(): HTMLElement | DocumentFragment {
     // @ts-ignore
