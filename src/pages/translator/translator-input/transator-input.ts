1c45bb9ef5ba97d0598ea8356613c47e9fb5d900
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-12 10:23:24 +0200

    feat: show typing count

--- src/pages/translator/translator-input/transator-input.ts
+++ src/pages/translator/translator-input/transator-input.ts
@@ -1,10 +1,13 @@
 import { LitElement, PropertyValues, html } from 'lit';
-import { customElement, query } from 'lit/decorators.js';
+import { customElement, query, state } from 'lit/decorators.js';
 
 @customElement('translator-input')
 export class TranslatorInput extends LitElement {
   @query('#input-textarea')
   inputTextarea!: HTMLTextAreaElement;
+
+  @state()
+  _textCount = 0;
 
   firstUpdated(_changedProperties: PropertyValues): void {
     super.firstUpdated(_changedProperties);
@@ -36,12 +39,16 @@ export class TranslatorInput extends LitElement {
         class="flex-1 focus-visible:outline-0 resize-none overflow-hidden"
         placeholder="Write what you want to translate here"
         maxlength="5000"></textarea>
-      <div >Optional</div>
+      <div class="flex justify-between">
+        <div class="">Controls</div>
+        <span class="opacity-50"> ${this._textCount} / 5000</span>
+      </div>
     </fieldset>`;
   }
 
   protected resizeTextarea() {
     this.inputTextarea.style.height = 'auto';
     this.inputTextarea.style.height = `${this.inputTextarea.scrollHeight}px`;
+    this._textCount = this.inputTextarea.value.length;
   }
 }
