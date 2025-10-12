a4a83107e8aaa30e7ea4b7679013981940f6f7d9
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-12 10:19:16 +0200

    style: set textarea

--- src/pages/translator/translator-input/transator-input.ts
+++ src/pages/translator/translator-input/transator-input.ts
@@ -1,14 +1,47 @@
-import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { LitElement, PropertyValues, html } from 'lit';
+import { customElement, query } from 'lit/decorators.js';
 
 @customElement('translator-input')
 export class TranslatorInput extends LitElement {
+  @query('#input-textarea')
+  inputTextarea!: HTMLTextAreaElement;
+
+  firstUpdated(_changedProperties: PropertyValues): void {
+    super.firstUpdated(_changedProperties);
+    this.inputTextarea.focus();
+    this.inputTextarea.addEventListener(
+      'input',
+      this.resizeTextarea.bind(this),
+    );
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this.inputTextarea.removeEventListener(
+      'input',
+      this.resizeTextarea.bind(this),
+    );
+  }
+
   protected createRenderRoot(): HTMLElement | DocumentFragment {
     // @ts-ignore
     return this;
   }
 
   render() {
-    return html`TranslatorInput works`;
+    return html` <fieldset class="flex flex-col h-full w-full">
+      <textarea
+        id="input-textarea"
+        name="input-textarea"
+        class="flex-1 focus-visible:outline-0 resize-none overflow-hidden"
+        placeholder="Write what you want to translate here"
+        maxlength="5000"></textarea>
+      <div >Optional</div>
+    </fieldset>`;
+  }
+
+  protected resizeTextarea() {
+    this.inputTextarea.style.height = 'auto';
+    this.inputTextarea.style.height = `${this.inputTextarea.scrollHeight}px`;
   }
 }
