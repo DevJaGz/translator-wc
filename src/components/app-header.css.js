66136f64e9ce6727904b03009749b23c529a5684
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-13 04:40:48 +0200

    style: update font size for google logo in main.css and adjust padding and font weight for header logo in app-header.css.js

--- src/components/app-header.css.js
+++ src/components/app-header.css.js
@@ -14,8 +14,8 @@ export const styles = css`
     font-weight: 500;
 
     .header__logo__traductor {
-      font-weight: 500;
-      padding-block-start: 0.25rem;
+      font-weight: 400;
+      padding-block-start: 0.5rem;
     }
   }
 
